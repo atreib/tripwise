d1e5661c6af488136f21015863e8fa087e4f8038
Andre Treib <andre.schumacher.treib@gmail.com>
2024-11-13 20:41:40 -0300

    chore: improve magic link template

--- src/lib/email/templates/login-magic-link.tsx
+++ src/lib/email/templates/login-magic-link.tsx
@@ -1,31 +1,102 @@
 import "server-only";
 
 import { appConstants } from "@/app/constants";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
 
-function MagicLinkTemplate(props: { name: string; url: string }) {
-  return (
-    <div>
-      <h1>Sign in on {appConstants.APP_NAME}</h1>
-      <h2>Hey {props.name} 👋🏻</h2>
-      <p>
-        Click the link below to sign in to your account on{" "}
-        <strong>{appConstants.APP_NAME}</strong>
-      </p>
-      <a href={props.url} target="_blank">
-        Sign in
-      </a>
-      <p>You are one step closer to planning your next trip!</p>
-      <p className="text-sm text-gray-500">
-        If the link above doesn&apos;t work, you can copy and paste the
-        following URL into your browser:
-      </p>
-      <p className="text-sm text-gray-500">{props.url}</p>
-      <p className="text-xs text-gray-500 mt-4">
-        If you didn&apos;t request this, please ignore this email. The link will
-        expire in 5 minutes.
-      </p>
-    </div>
-  );
-}
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "";
 
-export { MagicLinkTemplate };
+export const MagicLinkTemplate = (props: { name: string; url: string }) => (
+  <Html>
+    <Head />
+    <Preview>Log in with this magic link.</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img
+          src={`${baseUrl}/icons/logo-only.png`}
+          width={48}
+          height={48}
+          alt={appConstants.APP_NAME}
+        />
+        <Heading style={heading}>🪄 Your magic link</Heading>
+        <Section style={body}>
+          <Text style={paragraph}>
+            <Link style={link} href={props.url}>
+              👉 Click here to sign in 👈
+            </Link>
+          </Text>
+          <Text style={paragraph}>
+            If you didn&apos;t request this, please ignore this email.
+          </Text>
+        </Section>
+        <Text style={paragraph}>
+          Best,
+          <br />- {appConstants.APP_NAME} Team
+        </Text>
+        <Hr style={hr} />
+        <Img
+          src={`${baseUrl}/icons/logo-only.png`}
+          width={32}
+          height={32}
+          style={{
+            WebkitFilter: "grayscale(100%)",
+            filter: "grayscale(100%)",
+            margin: "20px 0",
+          }}
+        />
+      </Container>
+    </Body>
+  </Html>
+);
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 25px 48px",
+  backgroundImage: 'url("/assets/raycast-bg.png")',
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat, no-repeat",
+};
+
+const heading = {
+  fontSize: "28px",
+  fontWeight: "bold",
+  marginTop: "48px",
+};
+
+const body = {
+  margin: "24px 0",
+};
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+};
+
+const link = {
+  color: "#FF6363",
+};
+
+const hr = {
+  borderColor: "#dddddd",
+  marginTop: "48px",
+};
